2e6331053029ea90a3c347dd5fe3735d26fde2b4
monikagrz <55914238+monikagrz@users.noreply.github.com>
2022-01-28 14:44:03 +0900

    Added a link to the project on GitHub

--- src/App.js
+++ src/App.js
@@ -8,6 +8,17 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <SearchWeather />
+      <br />
+      <small>
+        <a
+          href="https://github.com/la-coccinelle/react-weather-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>{" "}
+        by Monika Grzegorczyk
+      </small>
     </div>
   );
 }
